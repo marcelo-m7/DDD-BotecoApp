ed94eef354412e42a89fb21e9bd82c26abc18eaf
Marcelo Santos <marcelo.net77@hotmail.com>
2025-11-24 20:48:17 +0000

    feat: implement BaseEntity class and extend domain classes for Product, DiningTable, Order, Receipt, and Production

--- abc/classes.py
+++ abc/classes.py
@@ -1,15 +1,37 @@
-class Product:
+
+
+class BaseEntity:
+    def __init__(self,
+                 id: int = None,
+                 name: str = None,
+                 description: str = None,
+                 created_at: str = None,
+                 updated_at: str = None):
+        pass
+    
+    def create(self):
+        pass
+    
+    def retrieve(self):
+        pass
+    
+    def update(self):
+        pass
+    
+    def delete(self):
+        pass
+
+class Product(BaseEntity):
     pass
 
-class DiningTable:
+class DiningTable(BaseEntity):
     pass
 
-class Order:
+class Order(BaseEntity):
     pass
 
-class Receipt:
+class Receipt(BaseEntity):
     pass
 
-class Production:
+class Production(BaseEntity):
     pass
-
